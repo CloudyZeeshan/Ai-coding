271680f2e5ea77ef73814d390e6da38b6b12a3bc
Zeeshan Haider <taimoorhassankhan2@gmail.com>
2025-06-30 16:35:38 +0500

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # Load the HTML file
 def load_html():
-    html_path = Path(__file__).parent / "static" / "index.html"
+    html_path = Path(__file__).parent / "codewithai" / "index.html"
     return html_path.read_text(encoding='utf-8')
 
 st.set_page_config(page_title="Delicious Food", layout="wide")
